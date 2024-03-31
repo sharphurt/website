33118b9fb5d0db47efe822529425021207f5e4fc
Лукоянов Павел <Pavel.Lukoianov@urfu.me>
2024-03-31 18:16:51 +0500

    feat: news block layout, games block layout, fragments

--- src/main/java/net/catstack/website/controller/PageController.java
+++ src/main/java/net/catstack/website/controller/PageController.java
@@ -1,6 +1,7 @@
 package net.catstack.website.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.catstack.website.dto.ShortGameInformationDto;
 import net.catstack.website.dto.ShortNewsInformationDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +16,19 @@ public class PageController {
     @GetMapping(value = {"/", "/index"})
     public String getIndexPage(Model model) {
         var newsCard = new ShortNewsInformationDto(
-                "uuid122333", "News HEader", "Новостить описание новости", "12 апреля 2023", "https://kakogo-chisla.ru/wp-content/uploads/2022/06/Den-morskoj-svinki.jpeg", "Морская свинка", "/fjsodjsodjs"
+                "News HEader", "Новостить описание новости", "12 апреля 2023", "https://kakogo-chisla.ru/wp-content/uploads/2022/06/Den-morskoj-svinki.jpeg", "Морская свинка", "/fjsodjsodjs"
         );
 
-        model.addAttribute("news", List.of(newsCard, newsCard, newsCard));
+        var gameCard = new ShortGameInformationDto(
+                "News HEader", "Новостить описание новости", "https://kakogo-chisla.ru/wp-content/uploads/2022/06/Den-morskoj-svinki.jpeg", "Морская свинка", List.of("google-play", "steam"), "/dsodjsodjsodj"
+        );
+        var gameCard1 = new ShortGameInformationDto(
+                "News HEader", "Новостить описание новости", "https://kakogo-chisla.ru/wp-content/uploads/2022/06/Den-morskoj-svinki.jpeg", "Морская свинка", List.of("steam"), "/dsodjsodjsodj"
+        );
 
+
+        model.addAttribute("news", List.of(newsCard, newsCard, newsCard));
+        model.addAttribute("games", List.of(gameCard, gameCard1, gameCard));
 
         return "index";
     }
--- src/main/java/net/catstack/website/dto/ShortGameInformationDto.java
+++ src/main/java/net/catstack/website/dto/ShortGameInformationDto.java
@@ -3,14 +3,16 @@ package net.catstack.website.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
-public class ShortNewsInformationDto {
+public class ShortGameInformationDto {
 
     private String title;
     private String description;
-    private String publicationDate;
     private String imageUrl;
     private String imageAlt;
-    private String newsUrl;
+    private List<String> gamePlatforms;
+    private String gameUrl;
 }
